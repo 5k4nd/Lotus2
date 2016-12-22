1243530f03c178aadf1cd490fc2595526dbf03b6
Bapt Abl <littledaddy@net-c.com>
2016-12-22 16:01:54 +0100

    ajout capacitif + début séquence (VLC)

--- audio_functions.py
+++ audio_functions.py
@@ -8,27 +8,8 @@ oui ce fichier est crade mais ça marche, et normalement vous devriez pas avoir
 from vlc import *
 from time import sleep
 
-
-
-def audio_battement(level):
-    player.set_media(media)
-    player.play()
-    if level==1:
-        sleep(.4)
-    elif level==2:
-        sleep(.5)
-    elif level==3:
-        sleep(.6)
-    elif level==4:
-        sleep(.7)
-    elif level==5:
-        sleep(.9)
-    elif level==6:
-        sleep(1)
-    elif level==7:
-        sleep(1.2)
-
-
+from threading import Thread
+from sys import exc_info
 
 
 try:
@@ -60,7 +41,8 @@ def pos_callback(event, player):
         sys.stdout.flush()
 
 
-filename = os.path.expanduser("data/audio/battement_punch.mp3")
+### CONSTRUCTION DES INSTANCES SONORES
+filename = os.path.expanduser("data/audio/battement/battement_double2.wav")
 
 # Need --sub-source=marq in order to use marquee below
 instance = Instance(["--sub-source=marq"] + sys.argv[1:])
@@ -71,40 +53,48 @@ except (AttributeError, NameError) as e:
                                            sys.argv[0], __version__,
                                            libvlc_get_version()))
     sys.exit(1)
-
-
 player = instance.media_player_new()
 
 
 
+filename2 = os.path.expanduser("data/audio/sequences/2_sirenes_1222.wav")
+instance2 = Instance(["--sub-source=marq"] + sys.argv[1:])
+try:
+    media2 = instance2.media_new(filename2)
+except (AttributeError, NameError) as e:
+    print('%s: %s (%s %s vs LibVLC %s)' % (e.__class__.__name__, e,
+                                           sys.argv[0], __version__,
+                                           libvlc_get_version()))
+    sys.exit(1)
+player2 = instance2.media_player_new()
 
-# Some marquee examples.  Marquee requires '--sub-source marq' in the
-# Instance() call above, see <http://www.videolan.org/doc/play-howto/en/ch04.html>
-# player.video_set_marquee_int(VideoMarqueeOption.Enable, 1)
-# player.video_set_marquee_int(VideoMarqueeOption.Size, 24)  # pixels
-# player.video_set_marquee_int(VideoMarqueeOption.Position, Position.Bottom)
-# if False:  # only one marquee can be specified
-#     player.video_set_marquee_int(VideoMarqueeOption.Timeout, 5000)  # millisec, 0==forever
-#     t = media.get_mrl()  # filename
-# else:  # update marquee text periodically
-#     player.video_set_marquee_int(VideoMarqueeOption.Timeout, 0)  # millisec, 0==forever
-#     player.video_set_marquee_int(VideoMarqueeOption.Refresh, 1000)  # millisec (or sec?)
-#     ##t = '$L / $D or $P at $T'
-#     t = '%Y-%m-%d  %H:%M:%S'
-# player.video_set_marquee_string(VideoMarqueeOption.Text, str_to_bytes(t))
 
-# Some event manager examples.  Note, the callback can be any Python
-# callable and does not need to be decorated.  Optionally, specify
-# any number of positional and/or keyword arguments to be passed
-# to the callback (in addition to the first one, an Event instance).
 
+
+def audio_battement(level):
+    player.set_media(media)
+    player.play()
+
+def audio_sequence(no):
+    if no==2:
+        player2.set_media(media2)
+        player2.play()
+
+def audio_stop(player_no):
+    player2.stop()
 
 if __name__ == '__main__':
     sleep(1)
-    while 1:
-        print("new loop!")
-        audio_battement(7)
-        sleep(.01)
+    # while 1:
+    try:
+        player2.set_media(media2)
+        player2.play()
+    except:
+        print exc_info()
+    # sleep(1.2)
+    # player2.set_media(media2)
+    # player2.play()
+
     event_manager = player.event_manager()
     event_manager.event_attach(EventType.MediaPlayerEndReached,      end_callback)
     event_manager.event_attach(EventType.MediaPlayerPositionChanged, pos_callback, player)
--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 #!env/bin/python2
 # -*- coding: utf8 -*-
 
+""" INFO DIVERSES
+
+par convention nous avons choisi :
+    - /dev/ttyACMO > MEGA (capteurs de distance)        à brancher en premier
+    - /dev/ttyACM1 > UNO (shield DMX)                   à brancher en deuxième
+    - /dev/ttyUSB0 > NANO (capteur capacitif du lotus)  à brancher en dernier
+
+
+IMPORTANT :
+    - au démarrage du système le capteur capacitif NE DOIT PAS ÊTRE TOUCHÉ
+
+"""
+
+
 from threading import Thread
 from time import sleep
 import serial
@@ -19,18 +33,69 @@ from effets import Effets
 import time
 
 
-
-
-class daemon_sensors(Thread):
+class daemon_capacitor(Thread):
     """
-        thread de l'arduino des capteurs de distance
+        thread de l'arduino du capacitif (NANO).
+        > le capacitif sur la nano du lotus se branche sur le pin 7
+
+
+    """
+    def __init__(self, ard_capacitor):
+        Thread.__init__(self)
+        self.ard_capacitor = ard_capacitor  # capteur capacitif du lotus
+        self.data = {
+            'capacitor_ground': -1,
+            'capacitor_value': -1
+        }
+        self.must_start_sequence = False
+
+
+    def run(self):
+        """
+        cette boucle tourne en boucle. peut être utile pour effectuer des
+        opérations périodiques (de la transformation de données en temps-réel
+            par exemple)
+        """
+        while 1:
+            sleep(.1)
+            try:
+                ### LISTEN FROM ARD_CAPACITOR (capacitif Lotus)
+                got = self.ard_capacitor.readline()
+                got = got.replace(" ", "")  # remove blanks
+                got = ast.literal_eval(got)  # cast str to dict
+
+                if "capa" in got.keys():
+                    # on ignore les 0.
+                    if (int(got['capa']) == 0):
+                        pass
+                    else:
+                        # on initialise le ground si ça n'a pas été déjà fait
+                        if (self.data['capacitor_ground'] == -1):
+                            self.data['capacitor_ground'] = int(got['capa'])
+                            print('[INFO] ground capacitif initialisé à %s' % self.data['capacitor_ground'])
+
+                        self.data['capacitor_value'] = int(got['capa'])
+                        # print self.data['capacitor_value']
+
+                if (self.data['capacitor_value'] > self.data['capacitor_ground']):
+                    self.must_start_sequence = True
+
+            except:
+                #quelle que soit l'erreur (formatage des donnees, valeurs aberrantes, etc.) on passe
+                pass
+
+
+class arduino_ultrasonics(Thread):
+    """
+        thread de
+            - l'arduino des capteurs de distance (MEGA)
+
     """
     def __init__(self, ard_sensors):
         Thread.__init__(self)
-        self.arduino = ard_sensors
+        self.ard_sensors = ard_sensors      # capteurs de ditance (ultrasonic sensors)
         self.data = {
-            'capt1': 0,
-            'volume': 1
+            'capt1': 239,
         }
 
 
@@ -38,22 +103,17 @@ class daemon_sensors(Thread):
         while 1:
             sleep(.01)
             try:
-                # listen from arduino
-                got = self.arduino.readline()
-                got = got.replace(" ", "")  # remove blanks
-                got = ast.literal_eval(got)  # cast str to dict
 
-                # looking for the data we want
-                if "axeX" in got.keys():
-                    self.data['axeX'] = int(got['axeX'])
-                    # self.core.logger.p_log('(axeX): ' + str(got['axeX']))
-                if "capt1" in got.keys():
-                    self.data['capt1'] = int(got['capt1'])
-                if "capteur2var" in got.keys():
-                    # insérer ici n'importe quoi :-)
-                    a = 42
-                if "ambiancevar" in got.keys():
-                    self.data['ambiancevar'] = int(got['ambiancevar'])
+                ### LISTEN FROM ARD_SENSORS (ulrasonic sensors)
+                got2 = self.ard_sensors.readline()
+                got2 = got2.replace(" ", "")  # remove blanks
+                got2 = ast.literal_eval(got2)  # cast str to dict
+
+                if "capt1" in got2.keys():
+                    self.data['capt1'] = int(got2['capt1'])
+                if "capt2" in got2.keys():
+                    self.data['capt2'] = int(got2['capt2'])
+
 
             except:
                 #quelle que soit l'erreur (formatage des donnees, valeurs aberrantes) on passe
@@ -65,10 +125,11 @@ class outputs_arduinos(Thread):
     """
         thread de l'arduino des sorties, pour l'instant du DMX.
     """
-    def __init__(self, ref_ard_dmx, arduino_sensors):
+    def __init__(self, ref_ard_dmx, arduino_sensors, arduino_capacitor):
         Thread.__init__(self)
-        self.ard_dmx=ref_ard_dmx
-        self.arduino_sensors=arduino_sensors
+        self.ard_dmx = ref_ard_dmx
+        self.arduino_sensors = arduino_sensors
+        self.ard_capacitor = arduino_capacitor
         """try:
             # mixer.init()
         except:
@@ -76,21 +137,31 @@ class outputs_arduinos(Thread):
             print exc_info()"""
 
     def run(self):
+        """
+        attention cette boucle contient des opérations bloquantes. ne pas y
+        placer d'opérations périodiques.
+        """
         while 1:
             sleep(.01)
-            effet = Effets(ard_dmx, ard_sensors)
+            effet = Effets(self.ard_dmx, self.arduino_sensors)
 
             try:
-                #distance = int(self.arduino_sensors.data['capt1'])
                 #tp1 = time.time()
-                effet.sequence()
-                #tp2 = time.time()
+
+                effet.battement_de_coeur()
+
 
                 #print(tp2 - tp1)
+
+                if self.ard_capacitor.must_start_sequence:
+                    effet.sequence()
+                    effet.sequence_stop()
+                    self.ard_capacitor.must_start_sequence = False
 
 
             except exc_info():
                 print exc_info()
+                print exc_info()[-1].tb_lineno
                 pass
 
         # ne pas toucher, c'est pour VLC !
@@ -103,33 +174,44 @@ class outputs_arduinos(Thread):
 
 if __name__ == '__main__':
     try:
-        # on ouvre le port d'écoute de l'arduino MEGA
-        # qui écoute les 8 ultrasons + le capteur capacitif
-
-        ard_sensors = serial.Serial('/dev/ttyUSB0', 115200)
-        #ard_sensors = "coucou"
-
-        # on ouvre le porte d'écoute de l'arduino DMX
-        ard_dmx = serial.Serial('/dev/ttyACM0', 115200)
-        #ard_dmx = "coucou"
+        # on ouvre le port d'écoute de l'arduino MEGA qui écoute les 8 ultrasons
+        ard_sensors = serial.Serial('/dev/ttyUSB2', 115200)
+        # ard_sensors = "coucou"
 
         # on démarre le thread d'écoute et de data processing de l'arduino MEGA
-        arduino_sensors = daemon_sensors(ard_sensors)
+        arduino_sensors = arduino_ultrasonics(ard_sensors)
         arduino_sensors.start()
 
-        # on temporise le temps que le thread MEGA démarre
+
+        # on ouvre le port d'écoute de l'arduino NANO qui écoute le lotus
+        ard_capacitor = serial.Serial('/dev/ttyACM0', 115200)
+        # ard_capacitor = "coucou"
+
+        # on démarre le thread d'écoute et de data processing de l'arduino NANO
+        arduino_capacitor = daemon_capacitor(ard_capacitor)
+        arduino_capacitor.start()
+
+
+
+        # on temporise le temps que le thread MEGA démarre et recoive ses premières données
         sleep(1)
 
+        # on ouvre le port d'écoute de l'arduino UNO-DMX
+        # ard_dmx = serial.Serial('/dev/ttyACM1', 115200)
+        ard_dmx = "coucou"
+
         # on démarre le thread qui gère l'arduino UNO DMX
-        arduino_senders = outputs_arduinos(ard_dmx, arduino_sensors)
+        arduino_senders = outputs_arduinos(ard_dmx, arduino_sensors, arduino_capacitor)
         arduino_senders.start()
-
-
-
 
     except:
         print 'problème d\'INIT'
         print exc_info()
+        print exc_info()[-1].tb_lineno
+        pass
+
+
+
 
     try:
         print ('APPUYER SUR ENTRÉE POUR QUITTER.')
@@ -142,7 +224,9 @@ if __name__ == '__main__':
     print('THE END.')
     # on arrête "proprement" les thread
     arduino_senders._Thread__stop()
+    arduino_capacitor._Thread__stop()
     arduino_sensors._Thread__stop()
     # on ferme "proprement" les ports d'écoute des serial arduino
     ard_dmx.close()
+    ard_capacitor.close()
     ard_sensors.close()
